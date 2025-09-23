b0eddadc487e87d57ee3789d6744afb60870afaa
Etienne Parmentier <etienne.Parmentier@heig-vd.ch>
2025-09-23 16:20:32 +0200

    Travail terminé

---
+++ rendu/rendu/hello.c
@@ -0,0 +1,10 @@
+/**
+* My first program in C
+* Author: Etienne Parmentier <Etienne.Parmentier@hello-world.com>
+**/
+#include <stdio.h>
+
+int main() {
+   printf("hello, world\n");
+   return 0;
+}
